427e04a0f1913f7f888fa2325ac9314a941ad6bd
nzkks <kkshanthosh@gmail.com>
2025-01-13 20:14:21 +1300

    Avoid prop drilling (unnecessary passing through the props in intermediate components) by using 'provide' and 'inject'

--- src/components/topics/KnowledgeTopics.vue
+++ src/components/topics/KnowledgeTopics.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="display: flex; gap: 2rem; margin: 0 auto; width: 100%; max-width: 40rem">
     <div style="flex: 1">
-      <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+      <knowledge-base @select-topic="activateTopic"></knowledge-base>
     </div>
 
     <div style="flex: 1">
@@ -35,6 +35,24 @@ export default {
       ],
       activeTopic: null,
     };
+  },
+  provide: {
+    topics: [
+      {
+        id: 'basics',
+        title: 'The Basics',
+        description: 'Core Vue basics you have to know',
+        fullText:
+          'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+      },
+      {
+        id: 'components',
+        title: 'Components',
+        description: 'Components are a core concept for building Vue UIs and apps',
+        fullText:
+          'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+      },
+    ],
   },
   methods: {
     activateTopic(topicId) {
