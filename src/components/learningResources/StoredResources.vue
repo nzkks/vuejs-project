92167c5af02693c0fae19fa6e43d95b44b10bfc1
nzkks <kkshanthosh@gmail.com>
2025-01-16 15:40:41 +1300

    StoredResources: add style

--- src/components/learningResources/StoredResources.vue
+++ src/components/learningResources/StoredResources.vue
@@ -12,3 +12,13 @@ export default {
   props: ['resources'],
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
