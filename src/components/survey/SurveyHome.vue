22564c91c0c1ea5b8ed8a1b1fe7d795f5a2130cc
nzkks <kkshanthosh@gmail.com>
2025-01-19 23:19:01 +1300

    comment out the local handling of the submitted form data and send the data to a Firebase database

--- src/components/survey/SurveyHome.vue
+++ src/components/survey/SurveyHome.vue
@@ -1,6 +1,8 @@
 <template>
-  <learning-survey @survey-submit="storeSurvey"></learning-survey>
-  <user-experiences :results="savedSurveyResults"></user-experiences>
+  <learning-survey></learning-survey>
+  <!-- <learning-survey @survey-submit="storeSurvey"></learning-survey> -->
+  <user-experiences></user-experiences>
+  <!-- <user-experiences :results="savedSurveyResults"></user-experiences> -->
 </template>
 
 <script>
@@ -12,21 +14,21 @@ export default {
     LearningSurvey,
     UserExperiences,
   },
-  data() {
-    return {
-      savedSurveyResults: [],
-    };
-  },
-  methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
-  },
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
 };
 </script>
