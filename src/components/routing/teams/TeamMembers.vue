8ff2b5d78f8ce0b4c09e59968d29a4fc5e9e1c02
nzkks <kkshanthosh@gmail.com>
2025-01-21 21:08:32 +1300

    Router example - Team Members: updating params data with watchers

--- src/components/routing/teams/TeamMembers.vue
+++ src/components/routing/teams/TeamMembers.vue
@@ -4,16 +4,21 @@
     <ul>
       <user-item v-for="member in members" :key="member.id" :name="member.fullName" :role="member.role"></user-item>
     </ul>
+
+    <!-- Example scenario -->
+    <RouterLink to="/teams/t2">Go to Team 2</RouterLink>
   </section>
 </template>
 
 <script>
+import { RouterLink } from 'vue-router';
 import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['teams', 'users'],
   components: {
     UserItem,
+    RouterLink,
   },
   data() {
     return {
@@ -21,20 +26,30 @@ export default {
       members: [],
     };
   },
+  methods: {
+    loadTeamMembers(route) {
+      const teamId = route.params.teamId;
+      const selectedTeam = this.teams.find(team => team.id === teamId);
+      const members = selectedTeam.members;
+
+      const selectedMembers = [];
+      for (const member of members) {
+        const selectedUser = this.users.find(user => user.id === member);
+        selectedMembers.push(selectedUser);
+      }
+      this.members = selectedMembers;
+      this.teamName = selectedTeam.name;
+    },
+  },
   created() {
     // this.$route.path.startsWith('/teams/t1');
-    const teamId = this.$route.params.teamId;
-    const selectedTeam = this.teams.find(team => team.id === teamId);
-    const members = selectedTeam.members;
-
-    const selectedMembers = [];
-    for (const member of members) {
-      const selectedUser = this.users.find(user => user.id === member);
-      selectedMembers.push(selectedUser);
-    }
-
-    this.members = selectedMembers;
-    this.teamName = selectedTeam.name;
+    this.loadTeamMembers(this.$route);
+  },
+  watch: {
+    // Vue router does not destroy and rebuild the loaded components when navigated around. It is more efficient to cache them when the URL changes. But the $route property always have the latest info on the loaded route and it will update when the URL changes. One way of handling this is to watch the $route property.
+    $route(newRoute) {
+      this.loadTeamMembers(newRoute);
+    },
   },
 };
 </script>
