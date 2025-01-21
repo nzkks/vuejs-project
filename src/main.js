505b6eade6f88f2c3cb72ac0e58908b0ea5b384b
nzkks <kkshanthosh@gmail.com>
2025-01-21 22:43:36 +1300

    main.js: instead of setting the component again as another route, use redirect to right route path

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ import TeamMembers from './components/routing/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: TeamsList },
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
 
