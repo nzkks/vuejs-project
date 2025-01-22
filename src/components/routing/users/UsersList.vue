30edda00f3d6d0773600de2439139c3a9dbf800d
nzkks <kkshanthosh@gmail.com>
2025-01-22 19:32:34 +1300

    Router example: in the component itself, beforeRouteLeave hook can be used to alert the user if any unfinished business left before leaving

--- src/components/routing/users/UsersList.vue
+++ src/components/routing/users/UsersList.vue
@@ -1,6 +1,9 @@
 <template>
   <!-- Example button to just show the "programmatic route navigation" -->
   <BaseButton @click="confirmInput">Confirm</BaseButton>
+
+  <!-- Example button to simulate unsaved changes prompt (confirm) before the user tries to navigate away -->
+  <BaseButton @click="updateChanges">Update Changes</BaseButton>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -14,6 +17,11 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
   methods: {
     confirmInput() {
       // Do something
@@ -22,11 +30,27 @@ export default {
       // this.$router.back();
       // this.$router.forward();
     },
+    updateChanges() {
+      this.changesSaved = true;
+    },
   },
   beforeRouteEnter(to, from, next) {
     console.log('UsersList Cmp beforeRouteEnter');
     // console.log({ to, from });
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList Cmp beforeRouteLeave');
+    // console.log({ to, from });
+    if (this.changesSaved) {
+      next();
+    } else {
+      confirm('Are you sure? You have unsaved changes!') ? next() : next(false);
+    }
+  },
+  unmounted() {
+    console.log('UsersList Cmp unmounted');
+    // here we cannot cancel the navigation. But beforeRouteLeave hook can be used to cancel the navigation
   },
 };
 </script>
