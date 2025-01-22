c960e3f3083d81269b94970267f5db4705e1fd32
nzkks <kkshanthosh@gmail.com>
2025-01-22 21:47:49 +1300

    animations example: add simple animation to a block

--- src/components/animationsAndTransitions/AnimationsAndTransitions.vue
+++ src/components/animationsAndTransitions/AnimationsAndTransitions.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <BaseDialogModal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,9 +15,12 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { animatedBlock: false, dialogIsVisible: false };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -34,6 +37,7 @@ export default {
   height: 8rem;
   background-color: #001533;
   margin-bottom: 2rem;
+  transition: transform 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -45,5 +49,9 @@ export default {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  transform: translateX(-150px);
 }
 </style>
