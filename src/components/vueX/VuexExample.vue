c4cb0aca2630fd865a7e52feaf0fc8c685365c55
nzkks <kkshanthosh@gmail.com>
2025-01-28 21:18:16 +1300

    VueX: instead of writing custom method names store actions, mapActions can be used

--- src/components/vueX/VuexExample.vue
+++ src/components/vueX/VuexExample.vue
@@ -1,29 +1,32 @@
 <template>
   <BaseContainer title="Vuex">
     <TheCounter />
-    <button @click="addOne">Increment</button>
-    <button @click="addTen">Increase by 10</button>
+    <button @click="increment">Increment</button>
+    <button @click="increaseBy10({ value: 10 })">Increase by 10</button>
   </BaseContainer>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 import BaseContainer from '../ui/BaseContainer.vue';
 import TheCounter from './TheCounter.vue';
 
 export default {
   components: { BaseContainer, TheCounter },
   methods: {
-    addOne() {
-      this.$store.dispatch('increment');
-    },
-    addTen() {
-      // this.$store.commit('increaseBy10', { value: 10 });
-      this.$store.dispatch({
-        type: 'increaseBy10', // 'type' property is mandatory as it the name of the mutation. But, of course, 'type' property won't be sent to the store.
-        value: 10,
-        // there can be any number of other properties
-      });
-    },
+    // addOne() {
+    //   this.$store.dispatch('increment');
+    // },
+    // addTen() {
+    //   // this.$store.commit('increaseBy10', { value: 10 });
+    //   this.$store.dispatch({
+    //     type: 'increaseBy10', // 'type' property is mandatory as it the name of the mutation. But, of course, 'type' property won't be sent to the store.
+    //     value: 10,
+    //     // there can be any number of other properties
+    //   });
+    // },
+    ...mapActions(['increment', 'increaseBy10']),
   },
 };
 </script>
