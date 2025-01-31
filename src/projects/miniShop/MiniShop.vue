8a5492e0297c30cb43f6f6078f2a5cca9c83aa69
nzkks <kkshanthosh@gmail.com>
2025-01-31 20:38:58 +1300

    MiniShop: Create VueX store module for Product

--- src/projects/miniShop/MiniShop.vue
+++ src/projects/miniShop/MiniShop.vue
@@ -5,21 +5,10 @@
 
 <script>
 import TheHeader from './components/nav/TheHeader.vue';
-import products from './products.js';
 
 export default {
   components: {
     TheHeader,
-  },
-  data() {
-    return {
-      products,
-    };
-  },
-  provide() {
-    return {
-      products: this.products,
-    };
   },
 };
 </script>
--- src/projects/miniShop/store/index.js
+++ src/projects/miniShop/store/index.js
@@ -5,9 +5,11 @@ import rootActions from './rootActions';
 import rootGetters from './rootGetters';
 
 import cartModule from './modules/cart/index';
+import productsModule from './modules/products';
 
 export default createStore({
   modules: {
+    prods: productsModule,
     cart: cartModule,
   },
   state() {
