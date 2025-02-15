43c3fa65bd319d4965eb0c65ebfb8e8a09d2e679
nzkks <kkshanthosh@gmail.com>
2025-02-16 11:54:27 +1300

    Composition API: lifecycle hooks

--- src/projects/compositionBasic/UserDetails.vue
+++ src/projects/compositionBasic/UserDetails.vue
@@ -1,5 +1,14 @@
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 
 export default {
   props: ['firstName', 'lastName'],
@@ -10,6 +19,18 @@ export default {
     context.emit('set-title', fullName.value);
 
     const age = inject('userAge');
+
+    // There are no lifesycle hooks like "beforeCreate" and "created" which can be found using the Options API. In composition API, the setup method is called when the component is created
+
+    // Rest of the available lifecycle hooks below including unmount related hooks
+    onBeforeMount(() => console.log('Before Mount'));
+    onMounted(() => console.log('Mounted'));
+    onBeforeUpdate(() => console.log('Before Update'));
+    onUpdated(() => console.log('Updated'));
+
+    // when the component is 'about to be removed' and 'removed'(example: v-if is used in the template in its parent component)
+    onBeforeUnmount(() => console.log('Before Unmount'));
+    onUnmounted(() => console.log('Unmounted'));
 
     return { userName: fullName, age };
   },
