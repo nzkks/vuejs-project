f369bef0af3a3082b34c74a0a36b4328c8ecb920
nzkks <kkshanthosh@gmail.com>
2025-02-16 21:04:36 +1300

    CompositionPrj01 - ProjectList: convert Options API to composition API

--- src/projects/compositionPrj01/components/projects/ProjectsList.vue
+++ src/projects/compositionPrj01/components/projects/ProjectsList.vue
@@ -1,4 +1,6 @@
 <script>
+import { computed, ref, watch, toRefs } from 'vue';
+
 import ProjectItem from './ProjectItem.vue';
 
 export default {
@@ -6,39 +8,44 @@ export default {
     ProjectItem,
   },
   props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter(prj => prj.title.includes(this.activeSearchTerm));
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const availableProjects = computed(() => {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter(prj => prj.title.includes(activeSearchTerm.value));
       }
-      return this.user.projects;
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
+      return props.user.projects;
+    });
+
+    const hasProjects = computed(() => {
+      return props.user.projects && availableProjects.value.length > 0;
+    });
+
+    const updateSearch = val => {
+      enteredSearchTerm.value = val;
+    };
+
+    watch(enteredSearchTerm, val => {
       setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+        if (val === enteredSearchTerm.value) {
+          activeSearchTerm.value = val;
         }
       }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
+    });
+
+    const { user } = toRefs(props);
+    watch(user, () => {
+      enteredSearchTerm.value = '';
+    });
+
+    return {
+      enteredSearchTerm,
+      availableProjects,
+      hasProjects,
+      updateSearch,
+    };
   },
 };
 </script>
