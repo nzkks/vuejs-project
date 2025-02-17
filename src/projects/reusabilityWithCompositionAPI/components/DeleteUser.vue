2347f3ff4274e9886813a12a8a40e01e6a9f1530
nzkks <kkshanthosh@gmail.com>
2025-02-17 17:47:26 +1300

    Composition API: composables / custom hooks

--- src/projects/reusabilityWithCompositionAPI/components/DeleteUser.vue
+++ src/projects/reusabilityWithCompositionAPI/components/DeleteUser.vue
@@ -9,28 +9,17 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert';
 
 export default {
   components: {
     UserAlert,
   },
   setup() {
-    const alertIsVisible = ref(false);
+    const { alertIsVisible, showAlert, hideAlert } = useAlert();
 
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-
-    return {
-      alertIsVisible,
-      showAlert,
-      hideAlert,
-    };
+    return { alertIsVisible, showAlert, hideAlert };
   },
 };
 </script>
